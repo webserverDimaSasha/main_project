545e679425a941bf90b2c8585c4e45884f3bd15a
DmitriyVyplov <DmitryVyplov@gmailcom>
2018-02-28 21:03:51 +0300

    Добавил миниигру
    Просто запускаешь .html-ку и там отгадываешь)

---
+++ Minigame/script.js
@@ -0,0 +1,5 @@
+var answer = prompt("Угадай породу собак");
+while(answer !== "бульдог" && answer !== "bulldog" && answer !== "english bulldog" && answer !== "английский бульдог" ) {
+	var answer = prompt ("Попробуй еще раз");
+}
+alert("Молодец ты угадал!!!");	
